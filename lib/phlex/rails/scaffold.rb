642a821cd6147866aeed7c1474305899c837faaa
Fagner Pereira Rosa <fagnerfpr@gmail.com>
2025-11-06 20:11:46 -0300

    first commit

---
+++ lib/phlex/rails/scaffold.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "scaffold/version"
+
+module Phlex
+  module Rails
+    module Scaffold
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
